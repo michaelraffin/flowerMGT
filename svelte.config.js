fae052de2de24b9a7b0c2a1b8f45f8085c173437
Michael <michaelraffinpaculba@gmail.com>
2024-11-26 16:50:00 +0800

    Uuse preview adapter

--- svelte.config.js
+++ svelte.config.js
@@ -12,22 +12,22 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
 
-    adapter: adapter({
-      edge: false,
-      split: true,
-    }),
-    // adapter: adapter(
-    //   adapter({
-    //     // default options are shown. On some platforms
-    //     // these options are set automatically — see below
-    //     pages: "build",
-    //     assets: "build",
-    //     fallback: null,
-    //     precompress: false,
-    //     strict: true,
-    //     out: "build",
-    //   }),
-    // ),
+    // adapter: adapter({
+    //   edge: false,
+    //   split: true,
+    // }),
+    adapter: adapter(
+      adapter({
+        // default options are shown. On some platforms
+        // these options are set automatically — see below
+        pages: "build",
+        assets: "build",
+        fallback: null,
+        precompress: false,
+        strict: true,
+        out: "build",
+      }),
+    ),
     alias: {
       "@/*": "./path/to/lib/*",
     },
