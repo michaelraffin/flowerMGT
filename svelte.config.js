8690e4f7da418492b7e80c667d82f8d08afce464
Michael <michaelraffinpaculba@gmail.com>
2024-11-26 22:50:57 +0800

    main Develop

--- svelte.config.js
+++ svelte.config.js
@@ -11,16 +11,18 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: false,
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false,
-    }),
+    adapter: adapter(
+      adapter({
+        // default options are shown. On some platforms
+        // these options are set automatically — see below
+        pages: "build",
+        assets: "build",
+        fallback: null,
+        precompress: false,
+        strict: true,
+        out: "build",
+      }),
+    ),
     alias: {
       "@/*": "./path/to/lib/*",
     },
