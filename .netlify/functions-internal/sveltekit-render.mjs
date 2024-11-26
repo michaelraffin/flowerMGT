d9478c7bf2e69bd7d87094a2f36f8b07b12b7b60
Michael <michaelraffinpaculba@gmail.com>
2024-11-26 18:43:52 +0800

    .netlify

--- .netlify/functions-internal/sveltekit-render.mjs
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -12,7 +12,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.JnoF7xb5.js","app":"_app/immutable/entry/app.HyXbkdDT.js","imports":["_app/immutable/entry/start.JnoF7xb5.js","_app/immutable/chunks/entry.DUuwr8ly.js","_app/immutable/chunks/runtime.cuZl10Db.js","_app/immutable/chunks/index.D0i4y2TH.js","_app/immutable/chunks/utils.BAN4fZiu.js","_app/immutable/entry/app.HyXbkdDT.js","_app/immutable/chunks/runtime.cuZl10Db.js","_app/immutable/chunks/store.CA-QW0ew.js","_app/immutable/chunks/disclose-version.D-MZTq7R.js","_app/immutable/chunks/utils.BAN4fZiu.js","_app/immutable/chunks/index-client.D1OmUi4l.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.DP9LVFvl.js","app":"_app/immutable/entry/app.7bSQmqWs.js","imports":["_app/immutable/entry/start.DP9LVFvl.js","_app/immutable/chunks/entry.BiX4kkLx.js","_app/immutable/chunks/runtime.cuZl10Db.js","_app/immutable/chunks/index.D0i4y2TH.js","_app/immutable/chunks/utils.BAN4fZiu.js","_app/immutable/entry/app.7bSQmqWs.js","_app/immutable/chunks/runtime.cuZl10Db.js","_app/immutable/chunks/store.CA-QW0ew.js","_app/immutable/chunks/disclose-version.D-MZTq7R.js","_app/immutable/chunks/utils.BAN4fZiu.js","_app/immutable/chunks/index-client.D1OmUi4l.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../server/nodes/0.js')),
 			__memo(() => import('../server/nodes/1.js')),
--- svelte.config.js
+++ svelte.config.js
@@ -1,61 +1,79 @@
+// // import adapter from "@sveltejs/adapter-netlify";
+// // import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+// // /** @type {import('@sveltejs/kit').Config} */
+// // const config = {
+// //   // Consult https://svelte.dev/docs/kit/integrations
+// //   // for more information about preprocessors
+// //   preprocess: vitePreprocess(),
+
+// //   kit: {
+// //     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// //     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// //     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+
+// //     // adapter: adapter({
+// //     //   edge: false,
+// //     //   split: true,
+// //     // }),
+// //     // target: "#svelte",
+// //     adapter: adapter(),
+// //     // adapter: adapter(
+// //     //   adapter({
+// //     //     // default options are shown. On some platforms
+// //     //     // these options are set automatically — see below
+// //     //     pages: "build",
+// //     //     assets: "build",
+// //     //     fallback: null,
+// //     //     precompress: false,
+// //     //     strict: true,
+// //     //     out: "build",
+// //     //   }),
+// //     // ),
+// //     alias: {
+// //       "@/*": "./path/to/lib/*",
+// //     },
+// //   },
+// // };
+
+// // export default config;
 // import adapter from "@sveltejs/adapter-netlify";
-// import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   // Consult https://svelte.dev/docs/kit/integrations
-//   // for more information about preprocessors
-//   preprocess: vitePreprocess(),
-
+// export default {
 //   kit: {
-//     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-//     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-//     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-
-//     // adapter: adapter({
-//     //   edge: false,
-//     //   split: true,
-//     // }),
-//     // target: "#svelte",
 //     adapter: adapter(),
-//     // adapter: adapter(
-//     //   adapter({
-//     //     // default options are shown. On some platforms
-//     //     // these options are set automatically — see below
-//     //     pages: "build",
-//     //     assets: "build",
-//     //     fallback: null,
-//     //     precompress: false,
-//     //     strict: true,
-//     //     out: "build",
-//     //   }),
-//     // ),
+//     adapter: adapter(
+//       adapter({
+//         // default options are shown. On some platforms
+//         // these options are set automatically — see below
+//         pages: "build",
+//         assets: "build",
+//         fallback: null,
+//         precompress: false,
+//         strict: true,
+//         out: "build",
+//       }),
+//     ),
 //     alias: {
 //       "@/*": "./path/to/lib/*",
 //     },
 //   },
 // };
-
-// export default config;
+//
 import adapter from "@sveltejs/adapter-netlify";
 
 export default {
   kit: {
-    adapter: adapter(),
-    adapter: adapter(
-      adapter({
-        // default options are shown. On some platforms
-        // these options are set automatically — see below
-        pages: "build",
-        assets: "build",
-        fallback: null,
-        precompress: false,
-        strict: true,
-        out: "build",
-      }),
-    ),
-    alias: {
-      "@/*": "./path/to/lib/*",
-    },
+    // default options are shown
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false,
+    }),
   },
 };
