d5d8092f5f5436fea0ad2cb5fec7c21cf6917008
Michael <michaelraffinpaculba@gmail.com>
2024-11-26 16:34:58 +0800

    New adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,9 +11,18 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      fallback: "200.html", // may differ from host to host
-    }),
+    adapter: adapter(
+      adapter({
+        // default options are shown. On some platforms
+        // these options are set automatically — see below
+        pages: "build",
+        assets: "build",
+        fallback: null,
+        precompress: false,
+        strict: true,
+        out: "build",
+      }),
+    ),
     alias: {
       "@/*": "./path/to/lib/*",
     },
