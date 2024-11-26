f812d7e51bc3670aefd637963a4c9f90f0add7a2
Michael <michaelraffinpaculba@gmail.com>
2024-11-26 22:55:10 +0800

    default svelte adpater

--- svelte.config.js
+++ svelte.config.js
@@ -11,18 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(
-      adapter({
-        // default options are shown. On some platforms
-        // these options are set automatically — see below
-        pages: "build",
-        assets: "build",
-        fallback: null,
-        precompress: false,
-        strict: true,
-        out: "build",
-      }),
-    ),
+    adapter: adapter(),
     alias: {
       "@/*": "./path/to/lib/*",
     },
