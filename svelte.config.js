b79ae6047b95b6f02de526fcfcce8ee76ebd29d7
Michael <michaelraffinpaculba@gmail.com>
2024-11-26 16:47:52 +0800

    FIX Dadapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,18 +11,23 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(
-      adapter({
-        // default options are shown. On some platforms
-        // these options are set automatically — see below
-        pages: "build",
-        assets: "build",
-        fallback: null,
-        precompress: false,
-        strict: true,
-        out: "build",
-      }),
-    ),
+
+    adapter: adapter({
+      edge: false,
+      split: true,
+    }),
+    // adapter: adapter(
+    //   adapter({
+    //     // default options are shown. On some platforms
+    //     // these options are set automatically — see below
+    //     pages: "build",
+    //     assets: "build",
+    //     fallback: null,
+    //     precompress: false,
+    //     strict: true,
+    //     out: "build",
+    //   }),
+    // ),
     alias: {
       "@/*": "./path/to/lib/*",
     },
