c8614eece76468584e19666f3579e46be7aafc05
Michael <michaelraffinpaculba@gmail.com>
2024-12-08 21:19:56 +0800

    Initial setup for svelte shadcn

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,53 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   // Consult https://svelte.dev/docs/kit/integrations
+//   // for more information about preprocessors
+//   preprocess: vitePreprocess(),
+//   kit: {
+//     adapter: adapter(),
+//     // ... other config
+//     alias: {
+//       "@/*": "./path/to/lib/*",
+//     },
+//   },
+//   // kit: {
+//   // 	// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+//   // 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+//   // 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+//   // 	adapter: adapter()
+//   // }
+// };
+
+// export default config;
+import adapter from "@sveltejs/adapter-auto";
+// import { vitePreprocess } from "@sveltejs/kit/vite";
+// import { vitePreprocess } from "@sveltejs/kit";
+// import preprocess from "svelte-preprocess";
+
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess({})],
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
-    alias: {
-      "@/*": "./path/to/lib/*",
-    },
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(
+      adapter({
+        // default options are shown. On some platforms
+        // these options are set automatically — see below
+        pages: "/build",
+        assets: "/build",
+        fallback: null,
+        precompress: false,
+        strict: true,
+        out: "/build",
+      }),
+    ),
   },
 };
 
